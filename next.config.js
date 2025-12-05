11e334721b7b9e7e4fbbc823bb1333fdd3eb0f1c
Infinitemarcus <marcus.natrielli@outlook.com>
2025-12-05 10:01:54 -0300

    chore: updating dependencies

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const createNextIntlPlugin = require("next-intl/plugin");
+const path = require("path");
 
-const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
+const withNextIntl = createNextIntlPlugin("./src/i18n.ts");
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  turbopack: {
+    root: path.join(__dirname),
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
